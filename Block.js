93ad672414e650011ecf4f8eb0b3f0d52e7e0d8a
Richardchrisostomo <95549377+Richardchrisostomo@users.noreply.github.com>
2022-06-29 11:40:17 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block{
+  constructor(x, y,width, height ) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    
+      this.image=loadImage("sprites/block.png");
+      this.visibility=255;
+    
+    }
+    display(){
+      var pos= this.body.position;
+      imageMode(CENTER);
+      image(this.image,pos.x,pos.y,this.width, this.height);
+    
+      if(this.body.speed<3){
+        // super.display();
+       }else{
+       push();
+        World.remove(world,this.body)  
+       this.visibility-=10;
+      tint(255,this.visibility)
+      image(this.image,this.body.position.x,this.body.position.y,25,25);
+      pop();
+    }
+    
+    
+    
+    }
+}
